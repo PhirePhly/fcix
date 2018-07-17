f822d52e00c7616f987d2aa49abb4bcb53b2979b
Kenneth Finnegan <kennethfinnegan2007@gmail.com>
2018-07-16 21:32:37 -0700

    Fixed broken sed command

--- bin/gen_routeserver_conf.sh
+++ bin/gen_routeserver_conf.sh
@@ -8,6 +8,6 @@ source ~/.virtualenvs/arouteserver/bin/activate
 arouteserver bird --ip-ver 4 -o RS1.bird.conf --cfg ./arouteserver.yml
 arouteserver bird --ip-ver 6 -o RS1.bird6.conf --cfg ./arouteserver.yml
 
-sed -i '/router id 206.80.238.253;/router id 206.80.238.253;/' <RS1.bird.conf >RS2.bird.conf
-sed -i '/router id 206.80.238.253;/router id 206.80.238.253;/' <RS1.bird6.conf >RS2.bird6.conf
+sed 's/router id 206.80.238.253/router id 206.80.238.254/' RS1.bird.conf >RS2.bird.conf
+sed 's/router id 206.80.238.253/router id 206.80.238.254/' RS1.bird6.conf >RS2.bird6.conf
 

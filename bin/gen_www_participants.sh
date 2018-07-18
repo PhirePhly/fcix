17ac1eee912ebecb31f107deda80aa6773ea7089
Kenneth Finnegan <kennethfinnegan2007@gmail.com>
2018-07-18 00:20:24 -0700

    Added link to route server policy to participants page

--- bin/gen_www_participants.sh
+++ bin/gen_www_participants.sh
@@ -7,3 +7,5 @@ awk -F '\t' '{print $2 " | [" $3 "](http://peerwith.me/" $3 ") | 206.80.238." $1
 
 echo "Route Server 1 | [33495](http://peerwith.me/33495) | 206.80.238.253 | 2001:504:91::253" >>www/participants.md
 echo "Route Server 2 | [33495](http://peerwith.me/33495) | 206.80.238.254 | 2001:504:91::254" >>www/participants.md
+echo "" >>www/participants.md
+echo "[Details on route server policies and community tags for route control](/rs-policy.html)" >>www/participants.md
